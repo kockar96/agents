d6fddc9fea237dcfc1423bc407f0015d9fd4cdf7
Aleksa Zivanovic <aleksazivanovick96@gmail.com>
2025-07-10 00:38:21 +0200

    add auth

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -31,6 +31,7 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authConfig = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
